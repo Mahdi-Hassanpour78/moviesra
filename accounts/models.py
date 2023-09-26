376ba22215e8ee417b6f10796b71fa3681de012f
Mahdi-Hassanpour78 <hassanpour.mahdi.mh@gmail.com>
2023-09-26 23:41:32 +0330

    add usermodel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class UsersModel(models.Model):
+    username = models.CharField(
+        max_length=20,
+        unique=True
+    )
+    email = models.EmailField(unique=True)
+    image = models.ImageField()
+    registery_date = models.DateTimeField(auto_now_add=True)
